05c7cb46392f42f9496821bf9bd2c72fd43fa9f2
Ralph Spandl <ralphspandl@Super-Ralph.local>
2022-06-27 21:08:11 -0400

    #8 map center marker and distance path

--- src/layers/infoLayer.js
+++ src/layers/infoLayer.js
@@ -1,4 +1,5 @@
 import * as d3 from 'd3';
+import mapboxgl from 'mapbox-gl';
 
 export const infoLayer = {
 
@@ -9,7 +10,7 @@ export const infoLayer = {
      */
     async create({ infoLayerData, MAPBOX_API, map }) {
         const infoFeatures = await infoLayer.getData({ infoLayerData, MAPBOX_API });
-        infoLayer.update.call(this, { infoLayerData, infoFeatures, map });
+        infoLayer.update.call(this, { infoLayerData, infoFeatures, map, MAPBOX_API });
     },
 
     /**
@@ -17,89 +18,140 @@ export const infoLayer = {
      * @infoLayerData {object} contains detailed info about features to draw
      * @infoFeatures {geoJSON} all features returned from API
      */
-    update({ infoLayerData, infoFeatures, map }) {
-        console.log({ infoLayerData, infoFeatures });
+    update({ infoLayerData, infoFeatures, map, MAPBOX_API }) {
+        // console.log({ infoLayerData, infoFeatures });
+
         const { visibleLayers } = infoLayerData;
 
         // Filter sub-categories
         const filterFeatures = (layer) => {
             const key = layer.selectionKey;
-            const groups = layer.selection.map((selection) => selection.group);
-            return infoFeatures.features.filter((d) => groups.includes(d.properties[key]));
+
+            const groups = layer && layer.icons
+                ? Object.keys(layer.icons).map((d) => d)
+                : [layer.layer];
+
+            const selectedFeatures = layer.icons && groups.length > 0
+                ? infoFeatures.features.filter((d) => groups.includes(d.properties[key]))
+                : infoFeatures.features.filter((d) => d.properties.tilequery.layer === layer.layer);
+
+            selectedFeatures.forEach((feature) => {
+                addIconProperties({ layer, feature });
+            });
+
+            return selectedFeatures;
+        };
+
+        const addIconProperties = ({ layer, feature }) => {
+            const key = layer.selectionKey;
+            const iconKey = feature.properties[key];
+            const icon = layer.icons ? layer.icons[iconKey] : `${iconKey}.png`;
+            feature.icon = infoLayerData.infoIconPath + icon;
         };
 
         // Filter only if sub categories are defined + combine features
-        const selectedFeatures = visibleLayers.map((layer) => {
-            const selection = layer.selection;
-            const features = selection && selection.length > 0
-                ? filterFeatures(layer)
-                : infoFeatures.features;
-            return features;
-        }).flat();
-
-        infoLayer.draw.call(this, { selectedFeatures, map });
+        const selectedFeatures = visibleLayers.map((layer) => filterFeatures(layer)).flat();
+        infoLayer.drawMarker.call(this, { infoLayerData, selectedFeatures, map, MAPBOX_API });
     },
+
+    /**
+     * draw markers on screen
+     * @selectedFeatures {geoJSON} array of features to be drawn on screen
+     * @map {object} the MapBox map object
+     */
+    drawMarker({ infoLayerData, selectedFeatures, map, MAPBOX_API }) {
+        const startLocation = `${infoLayerData.latitude},${infoLayerData.longitude}`;
+
+        const centerPoint = () => {
+            const el = document.createElement('div');
+            el.className = 'center';
+            el.style.backgroundImage = `url(${infoLayerData.infoIconPath}center.png)`;
+            el.style.backgroundSize = '100%';
+
+            new mapboxgl.Marker(el).setLngLat(startLocation.split(',')).addTo(map);
+        };
+
+        selectedFeatures.forEach((feature) => {
+            const el = document.createElement('div');
+            el.className = 'marker';
+            el.style.backgroundImage = `url(${feature.icon})`;
+            el.style.backgroundSize = '100%';
+
+            el.addEventListener('click', (event) => {
+                const properties = feature.properties;
+
+                map.MapCCV.appState = {
+                    type: 'user',
+                    value: 'click',
+                    message: `clicked on ${properties.name} (distance: ${properties.tilequery.distance}m)`,
+                    data: properties,
+                };
+
+                const endLocation = feature.geometry.coordinates;
+
+                infoLayer.drawPath({ map, startLocation, endLocation, MAPBOX_API });
+            });
+
+            /*
+            ! TO DO
+
+            * GUI: three configurations
+            * GUI: click on button get new data from map center
+            */
+
+            new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
+        });
+
+        centerPoint();
+    },
+
+    /*
+    ! To Do:
+    * function to remove all icons
+    */
 
     /**
      * draw features on screen
      * @selectedFeatures {geoJSON} array of features to be drawn on screen
      */
-    draw({ selectedFeatures, map }) {
-        console.log('selectedFeatures', selectedFeatures);
+    async drawPath({ startLocation, endLocation, map, MAPBOX_API }) {
+        const baseURL = 'https://api.mapbox.com/directions/v5/mapbox/walking/';
+        const pathQuery = `${baseURL}${startLocation};${endLocation}?geometries=geojson&access_token=${MAPBOX_API}`;
+        const pathInfo = await d3.json(pathQuery);
 
-        map.addSource('tilequery', {
-            type: 'geojson',
-            data: {
-                type: 'FeatureCollection',
-                features: selectedFeatures,
+        const data = pathInfo.routes[0];
+        const route = data.geometry.coordinates;
+        const geojson = {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'LineString',
+                coordinates: route,
             },
-        });
+        };
 
-        map.addLayer({
-            id: 'tilequery-points',
-            type: 'circle',
-            source: 'tilequery',
-            paint: {
-                'circle-stroke-color': 'white',
-                'circle-stroke-width': {
-                // Set the stroke width of each circle: https://docs.mapbox.com/mapbox-gl-js/style-spec/#paint-circle-circle-stroke-width
-                    stops: [
-                        [0, 0.1],
-                        [18, 3],
-                    ],
-                    base: 5,
+        // Reset route if already existing
+        if (map.getSource('route')) {
+            map.getSource('route').setData(geojson);
+        } else {
+            map.addLayer({
+                id: 'route',
+                type: 'line',
+                source: {
+                    type: 'geojson',
+                    data: geojson,
                 },
-                'circle-radius': {
-                // Set the radius of each circle, as well as its size at each zoom level: https://docs.mapbox.com/mapbox-gl-js/style-spec/#paint-circle-circle-radius
-                    stops: [
-                        [12, 7],
-                        [22, 180],
-                    ],
-                    base: 8,
+                layout: {
+                    'line-join': 'round',
+                    'line-cap': 'round',
                 },
-                'circle-color': '#eb8517',
-            },
-        });
-
-        map.on('click', 'tilequery-points', (event) => {
-            const properties = event.features[0].properties;
-            // console.log(properties);
-
-            map.MapCCV.appState = {
-                type: 'user',
-                value: 'click',
-                message: `clicked on ${properties.name}`,
-                data: properties,
-            };
-        });
-
-        map.on('mouseenter', 'tilequery-points', (event) => {
-            map.getCanvas().style.cursor = 'pointer'; // When the cursor enters a feature, set it to a pointer
-        });
-
-        map.on('mouseleave', 'tilequery-points', () => {
-            map.getCanvas().style.cursor = '';
-        });
+                paint: {
+                    'line-color': '#1c86a7',
+                    'line-width': 5,
+                    'line-opacity': 0.75,
+                },
+            });
+        }
     },
 
     async getData({ infoLayerData, MAPBOX_API }) {
